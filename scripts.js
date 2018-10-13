4034d45298c3d260bc3c29e7c905bcca6209861d
Gustavo Henrique Lima <drgustav1997@gmail.com>
2018-10-12 21:37:25 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,28 @@
+let paginas = new Array();
+
+
+$(document).ready(function() {
+  paginas.push('1');
+  console.log(paginas);
+  paginas.forEach(function(item) {
+    $('div#paginacao-botoes').append(
+        '<button type="button" class="btn btn-primary">' + item + '</button>');
+  });
+  $('div#paginacao-botoes').append('<button type="button" class="btn btn-primary">' + 'Próximo' + '</button>');
+  $('div#paginacao-botoes').append('<button type="button" class="btn btn-primary">' + 'Último' + '</button>');
+});
+
+$(document).ready(function() {
+
+  $('#paginacao-botoes .btn').on('click', function() {
+  //  alert($(this).val());
+    numPag = $(this).val();
+    switch (numPag) {
+      case "0":
+        $("p").append(document.createTextNode(numPag));
+        break;
+      case "1"?
+        $("p").append(document.createTextNode(numPag));
+    }
+  });
+});
