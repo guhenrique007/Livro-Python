c3051c6c5b73d35f665e5ad6131c9e8922fb0e10
Gustavo Henrique Lima <drgustav1997@gmail.com>
2018-10-12 21:53:59 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,12 +1,16 @@
 let paginas = new Array();
+let contador = 1;
 
 
 $(document).ready(function() {
   paginas.push('1');
   console.log(paginas);
   paginas.forEach(function(item) {
+    contador++;
     $('div#paginacao-botoes').append(
-        '<button type="button" class="btn btn-primary">' + item + '</button>');
+        //'<button type="button" class="btn btn-primary">' + item + '</button>');
+        '<button type="button" class="btn btn-primary" value=" ' + contador +' " >' + item + '</button>');
+        //o value dele n ta dando append no paragrafo
   });
   $('div#paginacao-botoes').append('<button type="button" class="btn btn-primary">' + 'Próximo' + '</button>');
   $('div#paginacao-botoes').append('<button type="button" class="btn btn-primary">' + 'Último' + '</button>');
@@ -18,10 +22,16 @@ $(document).ready(function() {
     alert($(this).val());
     numPag = $(this).val();
     switch (numPag) {
+      case "-1":
+        $("p").append(document.createTextNode(numPag));
+        break;
       case "0":
         $("p").append(document.createTextNode(numPag));
         break;
       case "1":
+        $("p").append(document.createTextNode(numPag));
+        break;
+      case "2":
         $("p").append(document.createTextNode(numPag));
         break;
     }
