a524a89e45a15995ab67334df64d6a0de788179f
Gustavo Henrique Lima <drgustav1997@gmail.com>
2018-12-09 22:38:30 -0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,15 +1,16 @@
 let paginas = new Array();
-let contador = 1;
-
+let contador = 0;
+let paginaAtual = 0;
 
 $(document).ready(function() {
   paginas.push('1');
+  paginas.push('2','3','4','5','6','7','8','9','10'); //refatorar isso aqui
   console.log(paginas);
   paginas.forEach(function(item) {
     contador++;
     $('div#paginacao-botoes').append(
         //'<button type="button" class="btn btn-primary">' + item + '</button>');
-        '<button type="button" class="btn btn-primary" value=" ' + contador +' " >' + item + '</button>');
+        '<button type="button" class="btn btn-primary" value="'+contador+'" >' + item + '</button>');
         //o value dele n ta dando append no paragrafo
   });
   $('div#paginacao-botoes').append('<button type="button" class="btn btn-primary">' + 'Próximo' + '</button>');
@@ -19,20 +20,34 @@ $(document).ready(function() {
 $(document).ready(function() {
 
   $('#paginacao-botoes .btn').on('click', function() {
-    alert($(this).val());
+  //alert($(this).val());
     numPag = $(this).val();
     switch (numPag) {
       case "-1":
-        $("p").append(document.createTextNode(numPag));
+        if(paginaAtual > 1){
+          var paginaAnterior = paginaAtual - 1;
+          paginaAnterior = paginaAnterior.toString();
+          paginaAtual = paginaAnterior;
+          console.log('pagina anterior: '+paginaAnterior);
+          var a = '.conteudo';
+          var globalDiv = $(a.concat(paginaAnterior)).html();
+          $('.divGlobal').html(globalDiv);
+        }
         break;
       case "0":
-        $("p").append(document.createTextNode(numPag));
+        paginaAtual = 1;
+        var globalDiv = $('.conteudo1').html();
+        $('.divGlobal').html(globalDiv);
         break;
       case "1":
-        $("p").append(document.createTextNode(numPag));
+        paginaAtual = 1;
+        var globalDiv = $('.conteudo1').html();
+        $('.divGlobal').html(globalDiv);
         break;
       case "2":
-        $("p").append(document.createTextNode(numPag));
+        paginaAtual = 2;
+        var globalDiv = $('.conteudo2').html();
+        $('.divGlobal').html(globalDiv);
         break;
     }
   });
